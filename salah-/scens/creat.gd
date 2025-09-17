6ba790d9ba4f873afd25687c69b7028967bb371f
gamezonxe <salaheldeen1232012@gmail.com>
2025-09-17 22:42:32 +0300

    Delete salah- directory

--- salah-/scens/creat.gd
+++
@@ -1,8 +0,0 @@
-
-extends Node
-
-@onready var shop_ui = $ShopUI   # غيّر المسار حسب مكان الـ UI عندك
-
-func _input(event):
-	if event.is_action_pressed("interact"): # زر E
-		shop_ui.visible = !shop_ui.visible
